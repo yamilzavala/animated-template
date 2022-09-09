a8dd785680ba3cbf3860da84253f51d1efda9d08
yamilzavala <zavalayamil9@gmail.com>
2022-09-09 14:59:03 -0300

    animated template project

---
+++ aos-init-.js
@@ -0,0 +1,6 @@
+AOS.init({    
+    delay: 10, // values from 0 to 3000, with step 50ms
+    duration: 1000, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    once: false,
+    });
